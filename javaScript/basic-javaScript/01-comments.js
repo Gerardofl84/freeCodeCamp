0d039c235a534ab4c1cfce4b318769c4346aae23
Gerardofl84 <63218699+Gerardofl84@users.noreply.github.com>
2023-09-26 15:33:45 -0500

    add first lessons on js

---
+++ javaScript/basic-javaScript/01-comments.js
@@ -0,0 +1,18 @@
+// this is the way we can make comments in javaScript
+
+// this is an in-line comment
+
+/*
+This is 
+a 
+multi-line
+comment
+*/
+
+/*
+
+Comments are lines of code that javaScript will intentionally ignonre
+
+Commetns are a great way to leave notes to yourself and to other people who will later need to figure out what that code does.
+
+*/
